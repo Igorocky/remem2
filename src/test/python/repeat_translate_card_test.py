37ff03db5b56149ddb490a069e47bb4a45c33e55
igorocky <123@gmail.com>
2024-04-19 21:01:34 +0200

    Update repeat_translate_card_test.py.

--- src/test/python/repeat_translate_card_test.py
+++ src/test/python/repeat_translate_card_test.py
@@ -15,20 +15,24 @@ from unittest import TestCase
 
 
 class ProcessUserInputTest(TestCase):
-    def test_make_initial_state_dir12(self) -> None:
-        # given
-        cache: Cache = CacheMock()  # type: ignore[assignment]
-        card = CardTranslate(
+    def _init_mocks(self):
+        self.cache: Cache = CacheMock()  # type: ignore[assignment]
+        self.card = CardTranslate(
             lang1_id=0, read_only1=0, text1='text1', tran1='tran1',
             lang2_id=1, read_only2=1, text2='text2', tran2='tran2')
-        task = Task(task_type_id=cache.task_types_si[TaskTypes.translate_12])
+
+    def test_make_initial_state_dir12(self) -> None:
+        # given
+        self._init_mocks()
+        task = Task(task_type_id=self.cache.task_types_si[TaskTypes.translate_12])
 
         # when
-        state = make_initial_state(cache, card, task)
+        state = make_initial_state(self.cache, self.card, task)
 
+        # then
         self.assertEqual(
             TranslateTaskState(
-                task=Task(task_type_id=cache.task_types_si[TaskTypes.translate_12]),
+                task=Task(task_type_id=self.cache.task_types_si[TaskTypes.translate_12]),
                 show_answer=False,
                 edit_card=False,
                 print_stats=False,
@@ -36,6 +40,35 @@ class ProcessUserInputTest(TestCase):
                 task_continuation=TaskContinuation.CONTINUE_TASK,
                 src=CardTranslateSide(lang_str='PL', read_only=0, text='text1', tran='tran1'),
                 dst=CardTranslateSide(lang_str='EN', read_only=1, text='text2', tran='tran2'),
+                first_user_translation=None,
+                user_translation=None,
+                correctness_indicator=None,
+                correct_translation_entered=False,
+                enter_mark=False,
+                err_msg=None,
+            ),
+            state
+        )
+
+    def test_make_initial_state_dir21(self) -> None:
+        # given
+        self._init_mocks()
+        task = Task(task_type_id=self.cache.task_types_si[TaskTypes.translate_21])
+
+        # when
+        state = make_initial_state(self.cache, self.card, task)
+
+        # then
+        self.assertEqual(
+            TranslateTaskState(
+                task=Task(task_type_id=self.cache.task_types_si[TaskTypes.translate_21]),
+                show_answer=False,
+                edit_card=False,
+                print_stats=False,
+                hist_rec=None,
+                task_continuation=TaskContinuation.CONTINUE_TASK,
+                src=CardTranslateSide(lang_str='EN', read_only=1, text='text2', tran='tran2'),
+                dst=CardTranslateSide(lang_str='PL', read_only=0, text='text1', tran='tran1'),
                 first_user_translation=None,
                 user_translation=None,
                 correctness_indicator=None,
