1ff96edcf59941d919ffdd6ef27298a73821d6dd
igorocky <123@gmail.com>
2024-04-21 13:23:56 +0200

    Update reepat/__init__.py.

--- src/main/python/remem/repeat/__init__.py
+++ src/main/python/remem/repeat/__init__.py
@@ -1,4 +1,3 @@
-import math
 import random
 from dataclasses import dataclass, field
 from enum import Enum
@@ -39,23 +38,15 @@ def select_random_tasks_from_beginning(
     src = sorted_tasks.copy()
     dst: list[TaskWithHist] = []
     while len(dst) < max_num_of_tasks and len(src) > 0:
-        time_span_for_search = math.fabs(src[0].last_repeated - src[-1].last_repeated) * 0.2
+        rnd_prefix_len = 3 if len(sorted_tasks) >= 5 else 2
         for folder_id in preferred_folders:
-            tasks_to_select_from = [
-                t for t in src
-                if (math.fabs(t.last_repeated - src[0].last_repeated) <= time_span_for_search
-                    and t.task.card.folder_id == folder_id)]
+            tasks_to_select_from = [t for t in src[:rnd_prefix_len] if t.task.card.folder_id == folder_id]
             if len(tasks_to_select_from) > 0:
                 selected_task = tasks_to_select_from[random.randint(0, len(tasks_to_select_from) - 1)]
                 break
         else:
-            tasks_to_select_from = [
-                t for t in src
-                if math.fabs(t.last_repeated - src[0].last_repeated) <= time_span_for_search]
-            if len(tasks_to_select_from) > 0:
-                selected_task = tasks_to_select_from[random.randint(0, len(tasks_to_select_from) - 1)]
-            else:
-                break
+            tasks_to_select_from = src[:rnd_prefix_len]
+            selected_task = tasks_to_select_from[random.randint(0, len(tasks_to_select_from) - 1)]
         dst.append(selected_task)
         src = [t for t in src if t.task.id != selected_task.task.id]
         preferred_folders.remove(selected_task.task.card.folder_id)
