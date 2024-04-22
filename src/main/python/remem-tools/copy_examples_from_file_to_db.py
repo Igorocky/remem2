4398e696140b43d3542ce4805844a8a58ef33789
igorocky <123@gmail.com>
2024-04-22 08:08:21 +0200

    Update copy_examples_from_file_to_db.py.

--- src/main/python/remem-tools/copy_examples_from_file_to_db.py
+++ src/main/python/remem-tools/copy_examples_from_file_to_db.py
@@ -15,34 +15,34 @@ def copy_examples_from_file_to_db(
     with open(file_with_examples, 'r', encoding='utf-8') as file:
         cnt = 0
         descr = None
-        form = None
-        word = None
-        example = None
+        notes = None
+        verb = None
+        text = None
         for line in file:
-            if line.startswith('t'):
+            if line.startswith('d'):
                 descr = line[1:].strip()
-                form = None
-                word = None
-                example = None
-            elif line.startswith('f'):
-                form = line[1:].strip()
-                word = None
-                example = None
-            elif line.startswith('w'):
-                word = line[1:].strip()
-                example = None
+                notes = None
+                verb = None
+                text = None
+            elif line.startswith('n'):
+                notes = line[1:].strip()
+                verb = None
+                text = None
+            elif line.startswith('v'):
+                verb = line[1:].strip()
+                text = None
             elif line.startswith('E'):
-                if example is not None:
-                    raise Exception(f'example is not None for {descr=} {form=} duplicated example: {line[1:].strip()}')
-                if word is None:
-                    raise Exception('word is None')
-                example = line[1:].strip()
-                example = example.replace(word, f'[[{word}]]')
-                print(f'{descr=} {form=} {word=} {example=}')
+                if text is not None:
+                    raise Exception(f'text is not None for {descr=} {notes=} duplicated text: {line[1:].strip()}')
+                if verb is None:
+                    raise Exception('verb is None')
+                text = line[1:].strip()
+                text = text.replace(verb, f'[[{verb}]]')
+                print(f'{descr=} {notes=} {verb=} {text=}')
                 db.execute(
                     f"""
-                    update CARD_FILL set text = :text where text = '' and descr = :descr and notes like '{form} %'
-                    """, {'text': example, 'descr': descr}
+                    update CARD_FILL set text = :text where text = '' and descr = :descr and notes = :notes
+                    """, {'text': text, 'descr': descr, 'notes': notes}
                 )
                 cnt += 1
     print(f'{cnt=}')
