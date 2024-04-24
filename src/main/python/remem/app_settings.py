21002163440be0ba193823f02235c4e3702d89c4
igorocky <123@gmail.com>
2024-04-24 12:10:05 +0200

    Reorder properties in AppSettings.

--- src/main/python/remem/app_settings.py
+++ src/main/python/remem/app_settings.py
@@ -8,19 +8,23 @@ from typing import Tuple
 class AppSettings:
     database_file: str = 'databases/remem.sqlite'
     database_backup_dir: str | None = None
-    database_schema_script_path: str = 'src/main/resources/schema_v1.sql'
+    buckets: dict[str, str] = field(default_factory=lambda: {
+        'short': '2m 5m 15m 30m',
+        'long': '1d 7d 30d',
+    })
+    dictionaries: dict[str, dict[str, str]] = field(default_factory=lambda: {
+        'ENG': {
+            'Cambridge': 'https://dictionary.cambridge.org/dictionary/english/{word}'
+        }
+    })
+
     console_colors_info: Tuple[int, int, int] = (30, 144, 255)
     console_colors_success: Tuple[int, int, int] = (26, 138, 59)
     console_colors_hint: Tuple[int, int, int] = (100, 100, 100)
     console_colors_prompt: Tuple[int, int, int] = (0, 0, 255)
     console_colors_error: Tuple[int, int, int] = (255, 0, 0)
     print_stack_traces_for_exceptions: bool = True
-    buckets: dict[str, str] = field(default_factory=lambda: {'short_buckets': '2m 5m 15m 30m'})
-    dictionaries: dict[str, dict[str, str]] = field(default_factory=lambda: {
-        'ENG': {
-            'Cambridge': 'https://dictionary.cambridge.org/dictionary/english/{word}'
-        }
-    })
+    database_schema_script_path: str = 'src/main/resources/schema_v1.sql'
 
 
 def load_app_settings(path: str) -> AppSettings:
